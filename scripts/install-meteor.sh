100744e5b4247c14fc13a5f9ada4c71f21e86556
Ernestas <ernestas.lobacevskis@stud.vgtu.lt>
2021-10-20 00:41:43 +0300

    Download specific meteor release script.

--- scripts/install-meteor.sh
+++ scripts/install-meteor.sh
@@ -9,7 +9,7 @@ if [ "$DEV_BUILD" = true ]; then
   curl -k -v https://install.meteor.com/ | sh
 else
   # download installer script
-  curl -k -v https://install.meteor.com -o /tmp/install_meteor.sh
+  curl -k -v "https://install.meteor.com/?release=\"$METEOR_VERSION\"/g" -o /tmp/install_meteor.sh
 
   # read in the release version in the app
   METEOR_VERSION=$(head $APP_SOURCE_DIR/.meteor/release | cut -d "@" -f 2)
